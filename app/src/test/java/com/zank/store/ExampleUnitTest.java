0533d7a4f3691c74b6621414f3d40548634323d6
Zank <741256459@qq.com>
2017-06-29 09:45:21 +0800

    修改领件测试类

--- app/src/test/java/com/zank/store/ExampleUnitTest.java
+++ app/src/test/java/com/zank/store/ExampleUnitTest.java
@@ -16,27 +16,23 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         List<String> list = new ArrayList<String>();
-        list.add("1456154211-3-2");
-        list.add("1-1456154211-3-1");
-        list.add("1-1456154211-3-2");
-        list.add("1-1456153211-3-1");
-        list.add("1-1456156211-3-2");
-        list.add("8746142316");
-        list.add("4653131535");
-        list.add("4153213221");
-        list.add("4561432174-3-1");
-        list.add("4561432174-3-2");
-        list.add("1-1465415321");
-        list.add("4654163136-2-1");
-        list.add("4654163136-2-2");
-        list.add("4654163136-3-1");
-        list.add("1451513131-1-1");
-        list.add("-");
-        list.add("456415311efewf");
-        list.add("fsdfsd-fwefwegs");
 
-        List<String> barCode = new ArrayList<>(); // 所有条码号
-        List<String> rBarCode = new ArrayList<>(); // 所有返单条码号
+        list.add("1-1456153289-3-1");
+
+        list.add("1-1456156245-3-2");
+
+        list.add("4561432174-3-1");
+        list.add("4564131321-1-1");
+        list.add("4564134561-2-1");
+        list.add("4564134561-2-2");
+        list.add("4561432174-3-2");
+        list.add("4564564561-510-2");
+        list.add("4654163136-2-1");
+        list.add("4654163136-3-1");
+        list.add("7456452114-3-2");
+
+        List<String> barCode = new ArrayList<String>(); // 所有条码号
+        List<String> rBarCode = new ArrayList<String>(); // 所有返单条码号
 
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).split("-").length == 3) { // 条码号
@@ -46,9 +42,9 @@ public class ExampleUnitTest {
                 rBarCode.add(list.get(i));
             }
         }
-        for (int j = 0; j < barCode.size(); j++) { // 条码号查重
-            List<String> cbarCode = new ArrayList<>(); // 某一条码号集合
-            String str = barCode.get(j).split("-")[0];
+        while (barCode.size() != 0) { // 条码号查重
+            List<String> cbarCode = new ArrayList<String>(); // 某一条码号集合
+            String str = barCode.get(0).split("-")[0];
             for (String code : barCode) { // 根据单号遍历出所有的条码号
                 if (code.contains(str)) {
                     cbarCode.add(code);
@@ -56,27 +52,25 @@ public class ExampleUnitTest {
             }
             barCode.removeAll(cbarCode); // 删除队列中的数据
             int count = 1;
-            if (count == cbarCode.size()) { // 1-1单
-                System.out.println("就一个啊：" + cbarCode.get(0));
-            } else {
-                for (String s : cbarCode) {
-                    int sp = Integer.parseInt(s.split("-")[1]);
-                    if (sp > count) {
-                        count = sp;
-                    }
+            for (String s : cbarCode) {
+                int sp = Integer.parseInt(s.split("-")[1]);
+                if (sp > count) {
+                    count = sp;
                 }
-                if (count > cbarCode.size()) {
-                    for (String s : cbarCode) {
-                        System.out.println("缺件：" + s + " 数量：" + (count - cbarCode.size()));
-                    }
+            }
+            if (count > cbarCode.size()) {
+                for (String s : cbarCode) {
+                    System.out.println("缺件：" + s + " 数量："
+                            + (count - cbarCode.size()));
                 }
             }
             System.out.println("---------------------------------------");
         }
 
-        for (int j = 0; j < rBarCode.size(); j++) { // 返单条码号查重
-            List<String> crBarCode = new ArrayList<>(); // 某一返单条码号集合
-            String str = rBarCode.get(j).split("-")[0]+"-"+rBarCode.get(j).split("-")[1];
+        while (rBarCode.size() != 0) { // 返单条码号查重
+            List<String> crBarCode = new ArrayList<String>(); // 某一返单条码号集合
+            String str = rBarCode.get(0).split("-")[0] + "-"
+                    + rBarCode.get(0).split("-")[1];
             for (String code : rBarCode) { // 根据单号遍历出所有的条码号
                 if (code.contains(str)) {
                     crBarCode.add(code);
@@ -84,21 +78,20 @@ public class ExampleUnitTest {
             }
             rBarCode.removeAll(crBarCode); // 删除队列中的数据
             int count = 1;
-            if (count == crBarCode.size()) { // 1-1单
-                System.out.println("就一个啊：" + crBarCode.get(0));
-            } else {
-                for (String s : crBarCode) {
-                    int sp = Integer.parseInt(s.split("-")[2]);
-                    if (sp > count) {
-                        count = sp;
-                    }
-                }
-                if (count > crBarCode.size()) {
-                    for (String s : crBarCode) {
-                        System.out.println("缺件：" + s + " 数量：" + (count - crBarCode.size()));
-                    }
+
+            for (String s : crBarCode) {
+                int sp = Integer.parseInt(s.split("-")[2]);
+                if (sp > count) {
+                    count = sp;
                 }
             }
+            if (count > crBarCode.size()) {
+                for (String s : crBarCode) {
+                    System.out.println("缺件：" + s + " 数量："
+                            + (count - crBarCode.size()));
+                }
+            }
+
             System.out.println("---------------------------------------");
         }
     }
